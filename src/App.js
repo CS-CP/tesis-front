7ca5276dcd9221b71117c2756c5635be74330d98
yops <cordova.cs@pucp.pe>
2020-10-18 16:27:54 -0500

    avance sabado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Layout, Menu } from 'antd';
+import DragFile from './components/DragFile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Footer, Content } = Layout;
+
+class App extends Component {
+  render() {
+    console.log("HOLA APP");
+    return (
+      <div>
+        <Layout className="layout">
+          <Header>
+            <div className="logo" />
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">Inicio</Menu.Item>
+              <Menu.Item key="2">Contáctanos</Menu.Item>
+            </Menu>
+          </Header>
+          <Content style={{ padding: '0 50px'}}>
+            <div class="cover d-flex justify-content-center align-items-center flex-column image">
+              <h1 style={{color: "floralwhite"}}> 
+                YOPS
+              </h1>
+            </div>
+            <DragFile></DragFile>
+            <div className="site-layout-content"></div>
+          </Content>
+
+          <Footer style={{ textAlign: 'center' }}>Trabajo de investigación PUCP 2020</Footer>
+        </Layout>
+      </div>
+    )
+  }
+
 }
 
 export default App;
+
+
